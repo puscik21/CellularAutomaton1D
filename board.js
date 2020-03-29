8fa9d4a8bafea408237f894a95439c8e5e46292a
Puscik21 <grzesiekpuscizna@gmail.com>
2020-03-30 01:21:16 +0200

    Added Start, Pause, Clear buttons

--- board.js
+++ board.js
@@ -5,30 +5,51 @@ var rules;
 var rowValues;
 var currentRow = 0;
 var timer = 0;
+var cellsPerRow;
+var isPaused = true;
 
 window.onload = function() {
     canvas = document.getElementById("board");
     context = canvas.getContext("2d");
-    let SQUARE_SIZE = canvas.height / 5;
-    let NUMBER_OF_SQUARES = 5 * 5;
-    console.log("Size of each square = " + SQUARE_SIZE + "px");
-
-    rowValues = [1, 0, 1, 0, 2, 1, 2, 1];       // TODO initialization
-    // TODO start button
-    if (!timer) {
-        timer = setTimeout(drawLine, 500);
-    }
-
     initRulesMap();
 };
 
-function drawLine() {
-    const squareSize = 50;
+function start() {
+    if (isPaused && document.getElementById('cellsPerRow').value !== '') {
+        cellsPerRow = document.getElementById('cellsPerRow').value;
+        initRowValues();
+        timer = setTimeout(drawLine, 500);
+        isPaused = false;
+    }
+}
 
+function initRowValues() {
+    rowValues = [];
+    for (let i = 0; i < cellsPerRow; i++) {
+        rowValues.push(getRandomInt(3));
+    }
+}
+
+function pause() {
+    if (!isPaused) {
+        clearTimeout(timer);
+        isPaused = true;
+    }
+}
+
+function clearCanvas() {
+    if (isPaused) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    currentRow = 0;
+}
+
+function drawLine() {
+    const squareSize = canvas.width / cellsPerRow;
     if ((currentRow + 1) * squareSize < canvas.height) {
         currentRow++;
     } else {
-        let data = context.getImageData(0, 50, canvas.width, canvas.height);
+        let data = context.getImageData(0, squareSize, canvas.width, canvas.height);
         context.putImageData(data, 0, 0);
     }
 
@@ -47,25 +68,63 @@ function drawLine() {
     }
 
     calculateRowValues();
-    if (timer) {
+    if (!isPaused) {
         timer = setTimeout(drawLine, 500);
     }
 }
 
 function calculateRowValues() {
-    rowValues = [];
-    for (let i = 0; i < 8; i++) {
-        rowValues.push(getRandomInt(3))
+    let temp = [];
+    let rowSize = rowValues.length;
+    for (let i = 1; i < rowSize - 1; i++) {
+        temp[i] = countCellValue('' + rowValues[i-1] + rowValues[i] + rowValues[i+1]);
     }
+    temp[0] = countCellValue('' + rowValues[rowSize-1] + rowValues[0] + rowValues[1]);
+    temp[rowSize - 1] = countCellValue('' + rowValues[rowSize-2] + rowValues[rowSize-1] + rowValues[0]);
+    rowValues = temp;
+}
+
+function countCellValue(ruleValue) {
+    return rules.get(ruleValue);
 }
 
 function initRulesMap() {
     let arr = [];
 
-    for (let i = 0; i < 27; i++) {
-        let ruleNumber = decimalToTrinary(i);
-        arr.push([ruleNumber, 1]);
-    }
+    // for (let i = 0; i < 27; i++) {
+    //     let ruleNumber = decimalToTrinary(i);
+    //     arr.push([ruleNumber, 1]);
+    // }
+
+    // for test purposes only
+    arr.push(['000', 0]);
+    arr.push(['001', 0]);
+    arr.push(['002', 0]);
+    arr.push(['010', 0]);
+    arr.push(['011', 0]);
+    arr.push(['012', 0]);
+    arr.push(['020', 0]);
+    arr.push(['021', 0]);
+    arr.push(['022', 0]);
+    arr.push(['100', 1]);
+    arr.push(['101', 1]);
+    arr.push(['102', 1]);
+    arr.push(['110', 1]);
+    arr.push(['111', 1]);
+    arr.push(['112', 1]);
+    arr.push(['120', 1]);
+    arr.push(['121', 1]);
+    arr.push(['122', 1]);
+    arr.push(['200', 2]);
+    arr.push(['201', 2]);
+    arr.push(['202', 2]);
+    arr.push(['210', 2]);
+    arr.push(['211', 2]);
+    arr.push(['212', 2]);
+    arr.push(['220', 2]);
+    arr.push(['221', 2]);
+    arr.push(['222', 2]);
+
     rules = new Map(arr);
 }
 
